638ec4505858d96c508906edfddd66d8e3daaaa1
S3RGIO-08 <60608827+S3RGIO-08@users.noreply.github.com>
2020-02-07 10:51:07 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,22 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+var Uno;
+
+var Dos;
+
+var resultado;
+
+Uno = document.getElementById("numeroUno").value ;
+Uno = parseInt(Uno)
+
+Dos = document.getElementById("numeroDos").value ;
+Dos = parseInt(Dos)
+
+resultado = (Uno + Dos);
+
+alert("La suma es " + resultado);
+
 
 }
 
--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,69 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+    var Uno;
+
+    var Dos; 
+
+    var resultado;
+
+    Uno = parseInt(document.getElementById("numeroUno").value);
+
+    Dos = parseInt(document.getElementById("numeroDos").value);
+
+    resultado = (Uno + Dos)
+
+   alert("La Suma es " + resultado);
+
 }
 
 function restar()
 {
-	
+    var Uno;
+
+    var Dos; 
+
+    var resultado;
+
+    Uno = parseInt(document.getElementById("numeroUno").value);
+
+    Dos = parseInt(document.getElementById("numeroDos").value);
+
+    resultado = (Uno - Dos)
+
+   alert("La Resta es " + resultado);
 }
 
 function multiplicar()
 { 
-	
+    var Uno;
+
+    var Dos; 
+
+    var resultado;
+
+    Uno = parseInt(document.getElementById("numeroUno").value);
+
+    Dos = parseInt(document.getElementById("numeroDos").value);
+
+    resultado = (Uno * Dos)
+
+   alert("La Multiplicacíon es " + resultado);
 }
 
 function dividir()
 {
-	
-}
+    var Uno;
 
+    var Dos; 
+
+    var resultado;
+
+    Uno = parseInt(document.getElementById("numeroUno").value);
+
+    Dos = parseInt(document.getElementById("numeroDos").value);
+
+    resultado = (Uno / Dos)
+
+   alert("La Divisíon es " + resultado);
+}
